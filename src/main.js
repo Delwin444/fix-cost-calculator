d7d48d904d708bab32440baadff6f52428b4f8e2
Delwin444 <justinkuhn@t-online.de>
2020-10-06 21:39:24 +0200

    Include collapsible plugin from bootstrap-vue.

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VueGoogleCharts from 'vue-google-charts'
+import { CollapsePlugin } from 'bootstrap-vue'
 
 Vue.use(VueGoogleCharts)
+Vue.use(CollapsePlugin)
 
 Vue.config.productionTip = false
 
