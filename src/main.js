f674264af14eec13459deebb39b88e18389abc73
Justin Kuhn <justinkuhn@t-online.de>
2021-06-27 21:56:48 +0200

    Add sweet alert 2 vuejs wrapper

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import router from './router'
 import store from './store'
 import VueGoogleCharts from 'vue-google-charts'
 import { CollapsePlugin } from 'bootstrap-vue'
+import VueSweetalert2 from 'vue-sweetalert2'
+
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 Vue.use(VueGoogleCharts)
 Vue.use(CollapsePlugin)
+Vue.use(VueSweetalert2)
 
 Vue.config.productionTip = false
 
